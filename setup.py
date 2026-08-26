3df7a337a852f0b432f73c4892c4b48590a01bd0
Ilia Choly <ilia.choly@gmail.com>
2026-08-26 09:55:28 -0400

    Version 0.11

--- setup.py
+++ setup.py
@@ -6,8 +6,8 @@ setup(
     url          = 'https://github.com/icholy/durationpy',
     author       = 'Ilia Choly',
     author_email = 'ilia.choly@gmail.com',
-    download_url = 'https://github.com/icholy/durationpy/tarball/0.10',
-    version      = '0.10',
+    download_url = 'https://github.com/icholy/durationpy/tarball/0.11',
+    version      = '0.11',
     packages     = ['durationpy'],
     package_data = {'durationpy': ['py.typed', '*.pyi']},
     license      = 'MIT'
